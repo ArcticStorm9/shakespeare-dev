39b971dbae07bbec4c5eac5d6953669c1ba752d6
Ian <irshakes11@gmail.com>
2023-01-08 22:24:37 -0700

    i hate doing this

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from 'svelte-adapter-static';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 const dev = process.argv.includes('dev');
